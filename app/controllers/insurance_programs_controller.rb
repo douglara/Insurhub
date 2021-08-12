bda686d68635b7e1c102c225c0ffa3d5819cfe2f
Douglas Lara <douglasdelaras@gmail.com>
2021-08-12 04:25:19 -0300

    Fix Unsplash quota reached

--- app/controllers/insurance_programs_controller.rb
+++ app/controllers/insurance_programs_controller.rb
@@ -3,12 +3,16 @@ class InsuranceProgramsController < ApplicationController
 
   # GET /insurance_programs/1 or /insurance_programs/1.json
   def show
-    @category_phone_url = Unsplash::Photo.random(query: "#{@insurance_program.insurance_category.name}").urls.regular
+    @category_phone_url = get_background_picture_url
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_insurance_program
       @insurance_program = InsuranceProgram.find(params[:id])
+    end
+
+    def get_background_picture_url
+      Unsplash::Photo.random(query: "#{@insurance_program.insurance_category.name}").urls.regular rescue '/imgs/insurance-background.jpeg'
     end
 end
